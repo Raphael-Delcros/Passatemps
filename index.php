cded6ebdeb8a4bc68ffa1886ecadfa91c2ea3653
Naia Mondeteguy <Naia Mondeteguy>
2025-11-10 15:02:11 +0100

    Classe Jeu

--- index.php
+++ index.php
@@ -2,52 +2,9 @@
 
 // Ajout du code commun à toutes les pages
 require_once 'include.php';
-
-<<<<<<< HEAD
-
-
-$template = $twig->load('index.html.twig');
-echo $template->render();
-/*try{
-    if (isset($_GET['controleur'])){
-    $controleurName =$_GET['controleur'] ;
-    }
-    else{
-        $controleurName = "" ;
-    }
-
-    if (isset($_GET['methode'])){
-        $methode =$_GET['methode'] ;
-    }
-    else{
-        $methode = "" ;
-    }
-
-    if ($controleurName == "" && $methode == ""){
-        var_dump("ici");
-        $controleurName = "Categorie" ;
-        $methode = "lister" ;
-    }
-
-    if ( $controleurName =="" ) {
-        throw new Exception("Le controleur n'est pas defini");
-    }
-    if ( $methode == "" ) {
-        throw new Exception("Le methode n'est pas defini");
-    }
-}
-catch(Exception $e){
-    die("Erreur : ".$e->getMessage());
-}
-
-$controleur=ControllerFactory::getController($controleurName, $loader, $twig);
-
-$controleur->call($methode);
-*/
 /*
-=======
->>>>>>> a8e75ce0b2cd1f737e1e212be500a06db17eb660
 try {
+
     // Récupération du contrôleur
     if (isset($_GET['controleur'])) {
         $controllerName = $_GET['controleur'];
@@ -90,7 +47,7 @@ try {
 } catch (Exception $e) {
     die("Erreur : " . $e->getMessage());
 }
-    */
+*/
 
 // test pour les jeux
 
