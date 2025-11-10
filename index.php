50a3ec10c9942c10ff4463edcc8c929091aba10b
IrazoquiIon <iirazoqui001@iutbayonne.univ-pau.fr>
2025-11-10 08:34:54 +0100

    Ajustement de l'index pour utiliser factory

--- index.php
+++ index.php
@@ -1,108 +1,48 @@
 <?php
 
-//Ajout du code commun à toutes les pages
+// Ajout du code commun à toutes les pages
 require_once 'include.php';
 
-
-
-$template = $twig->load('index.html.twig');
-echo $template->render();
-/*try{
-    if (isset($_GET['controleur'])){
-    $controleurName =$_GET['controleur'] ;
-    }
-    else{
-        $controleurName = "" ;
-    }
-
-    if (isset($_GET['methode'])){
-        $methode =$_GET['methode'] ;
-    }
-    else{
-        $methode = "" ;
-    }
-
-    if ($controleurName == "" && $methode == ""){
-        var_dump("ici");
-        $controleurName = "Categorie" ;
-        $methode = "lister" ;
-    }
-
-    if ( $controleurName =="" ) {
-        throw new Exception("Le controleur n'est pas defini");
-    }
-    if ( $methode == "" ) {
-        throw new Exception("Le methode n'est pas defini");
-    }
-}
-catch(Exception $e){
-    die("Erreur : ".$e->getMessage());
-}
-
-$controleur=ControllerFactory::getController($controleurName, $loader, $twig);
-
-$controleur->call($methode);
-*/
-
-// try {
-//     $controleurName = isset($_GET['controller']) ? $_GET['controller'] : 'compte';
-//     $methode = isset($_GET['methode']) ? $_GET['methode'] : 'lister';
-
-//     if ($controleurName == "") {
-//         throw new Exception("Le controleur n'est pas défini");
-//     }
-//     if ($methode == "") {
-//         throw new Exception("La méthode n'est pas définie");
-//     }
-// } catch (Exception $e) {
-//     die("Erreur : " . $e->getMessage());
-// }
-
-// // Instanciation du controller selon le nom
-// switch (strtolower($controleurName)) {
-//     case 'compte':
-//         $controleur = new ControllerCompte($twig, $loader);
-//         break;
-//     // tu peux ajouter d'autres controllers ici
-//     default:
-//         die("Controller inconnu : $controleurName");
-// }
-
-// // Appel de la méthode
-// if (method_exists($controleur, $methode)) {
-//     $controleur->$methode();
-// } else {
-//     die("Méthode inconnue : $methode");
-// }
 try {
-    // Récupération du controller et de la méthode depuis l'URL
-    $controllerName = isset($_GET['controller']) ? $_GET['controller'] : 'signalement';
-    $methode = isset($_GET['methode']) ? $_GET['methode'] : 'lister';
-
-    if ($controllerName == "") {
-        throw new Exception("Le controller n'est pas défini");
+    // Récupération du contrôleur
+    if (isset($_GET['controleur'])) {
+        $controllerName = $_GET['controleur'];
+    } else {
+        $controllerName = '';
     }
-    if ($methode == "") {
+
+    // Récupération de la méthode
+    if (isset($_GET['methode'])) {
+        $methode = $_GET['methode'];
+    } else {
+        $methode = '';
+    }
+
+    // Gestion de la page d'accueil par défaut
+    if ($controllerName == '' && $methode == '') {
+        $controllerName = 'signalement'; 
+        $methode = 'lister';
+    }
+
+    if ($controllerName == '') {
+        throw new Exception("Le contrôleur n'est pas défini");
+    }
+
+    if ($methode == '') {
         throw new Exception("La méthode n'est pas définie");
     }
 
-    // Instanciation du controller selon le nom
-    switch (strtolower($controllerName)) {
-        case 'signalement':
-            $controller = new ControllerSignalement($twig, $loader);
-            break;
-        default:
-            throw new Exception("Controller inconnu : $controllerName");
+    // Création du contrôleur via la factory
+    $controller = ControllerFactory::getController($controllerName, $loader, $twig);
+
+    // Vérifie que la méthode existe bien
+    if (!method_exists($controller, $methode)) {
+        throw new Exception("La méthode '$methode' n'existe pas dans le contrôleur '$controllerName'");
     }
 
-    // Vérification et appel de la méthode
-    if (method_exists($controller, $methode)) {
-        $controller->$methode();
-    } else {
-        throw new Exception("Méthode inconnue : $methode");
-    }
+    // Appel de la méthode
+    $controller->call($methode);
 
 } catch (Exception $e) {
-    echo "<h1>Erreur :</h1>";
-    echo "<p>" . $e->getMessage() . "</p>";
+    die("Erreur : " . $e->getMessage());
 }
