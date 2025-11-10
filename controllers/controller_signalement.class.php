b983ab45fdfc2d409c5871254851d8634c8af851
IrazoquiIon <iirazoqui001@iutbayonne.univ-pau.fr>
2025-11-10 10:40:32 +0100

    Création page d'erreur + utilisation sur les signalements

--- controllers/controller_signalement.class.php
+++ controllers/controller_signalement.class.php
@@ -18,14 +18,41 @@ class ControllerSignalement extends Controller {
     }
 
     public function afficher() {
-        $id = isset($_GET['id']) ? intval($_GET['id']) : null;
+    try {
+        // Vérifie la présence et la validité de l'ID
+        if (!isset($_GET['id']) || empty($_GET['id'])) {
+            throw new Exception("Aucun ID de signalement fourni.", 400);
+        }
+
+        $id = intval($_GET['id']);
+        if ($id <= 0) {
+            throw new Exception("L'ID fourni est invalide.", 400);
+        }
+
+        // Récupération du signalement
         $dao = new SignalementDao($this->getPdo());
         $signalementAssoc = $dao->findAssoc($id);
+
+        if (!$signalementAssoc) {
+            throw new Exception("Aucun signalement trouvé avec l'ID $id.", 404);
+        }
+
         $signalement = $dao->hydrate($signalementAssoc);
 
+        // Affichage du template normal
         $template = $this->getTwig()->load('signalement.html.twig');
         echo $template->render([
             'signalement' => $signalement,
         ]);
+
+    } catch (Exception $e) {
+        // Si erreur → affichage de la page d’erreur Twig
+        $template = $this->getTwig()->load('erreur.html.twig');
+        echo $template->render([
+            'message' => $e->getMessage(),
+            'code' => $e->getCode() ?: 500
+        ]);
     }
+}
+
 }
