9a7de71aff38338aa235deb55d65b0d14ccc9245
Naia Mondeteguy <nmondeteguy@iutbayonne.univ-pau.fr>
2025-11-17 16:38:34 +0100

    formulaire de vente, ajout des fonctions dans annonces dao et dans le controlleur

--- controllers/controller_vendre.class.php
+++ controllers/controller_vendre.class.php
@@ -17,13 +17,26 @@ class ControllerVendre extends controller
    {
         // bla bla bla faut faire le formulaire bouh bouh créer une annonce tout ca tout ca
         extract($_GET,EXTR_OVERWRITE);
-        var_dump($_GET);
-
        // Logique pour confirmer la vente (par exemple, enregistrer les données dans la base de données)
-       $template = $this->getTwig()->load('vendre.html.twig');
-       echo $template->render(
+        $template = $this->getTwig()->load('vendre.html.twig');
+        echo $template->render();
 
-       );
+        $date= getdate();
+        $stringdate =  $date['year'] . '-' . $date['mon'] . '-' . $date['mday'];
+        
+        $dao = new AnnonceDao($this->getPdo());
+        $idAnnonce = $dao->lastId() + 1;
+
+        $annonce = new Annonce($idAnnonce, $_GET['titre'], $_GET['description'], $_GET['prix'], $stringdate, $_GET['etatJeu'], 'achete', null, null);
+        //à changer IdCompteVendeur quand les comptes seront faits
+        //Faire id jeu quand la recherche et la récupération d'id serai faite
+
+        $result = $dao->InsertInto($annonce);
+        if ($result) {
+            echo "Annonce insérée avec succès.";
+        } else {
+            echo "Erreur lors de l'insertion de l'annonce.";
+        }
    }
 
 }
