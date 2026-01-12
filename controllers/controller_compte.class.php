93a8ffd9a691b780d74c2d9133904040cb511619
Raphaël DELCROS <delcros.raphael1@gmail.com>
2026-01-12 13:01:35 +0100

    Fixe merge ENCORE

--- controllers/controller_compte.class.php
+++ controllers/controller_compte.class.php
@@ -132,7 +132,6 @@ class ControllerCompte extends Controller
             $dao = new CompteDao($this->getPdo());
             $compte = new Compte(null, $nom, $prenom, $email, $password, null, null, 'utilisateur');
             $dao->insert($compte);
-            header('Location: index.php?controleur=connexion&methode=connexion');
         } else {
             // Afficher les erreurs
             $this->afficherErreursInscription($messagesErreurs);
