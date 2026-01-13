ca7d2967c08ed96ae489a2deff646751b0ae01dc
Naia Mondeteguy <nmondeteguy@iutbayonne.univ-pau.fr>
2026-01-13 09:24:13 +0100

    mering de commande

--- controllers/controller_livraison.class.php
+++ controllers/controller_livraison.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file controller_livraison.class.php
  * @author Raphaël Delcros
@@ -31,18 +32,19 @@ class ControllerLivraison extends Controller
      */
     public function afficher()
     {
-        $id = isset($_GET['id']) ? intval($_GET['id']) : null;
-        $dao = new LivraisonDao($this->getPdo());
-        $commande = $dao->find($id);
+        if (!isset($_SESSION['idCompte'])) {
+            $template = $this->getTwig()->load('connexion.html.twig');
+            echo $template->render();
+            return;
+        } else {
+            $dao = new LivraisonDao($this->getPdo());
 
-        // On récupère le titre & prix du jeu pour cette commande
-        $annonce = $dao->getInfoAnnonceByLivraisonId($commande->getIdLivraison());
-
-        $template = $this->getTwig()->load('livraison.html.twig');
-        echo $template->render([
-            'commande' => $commande,
-            'annonce' => $annonce,
-        ]);
+            $commande = $dao->findAllAssocFromIdWithAnnonceInfo($_SESSION['idCompte'], isset($_GET['id']) ? intval($_GET['id']) : null);
+            $template = $this->getTwig()->load('livraison.html.twig');
+            echo $template->render([
+                'commande' => $commande[0],
+            ]);
+        }
     }
 
     /**
@@ -52,17 +54,17 @@ class ControllerLivraison extends Controller
      */
     public function lister()
     {
-        $dao = new LivraisonDao($this->getPdo());
-        $commandes = $dao->findAllAssoc(); // récupère toutes le commandes en tableau associatif
-
-        // On récupère le titre & prix du jeu pour chaque commande
-        foreach ($commandes as $commande) {
-            $commande['jeuCommande'] = $dao->getInfoAnnonceByLivraisonId($commande['idLivraison']);
+        if (!isset($_SESSION['idCompte'])) {
+            $template = $this->getTwig()->load('connexion.html.twig');
+            echo $template->render();
+            return;
+        } else {
+            $dao = new LivraisonDao($this->getPdo());
+            $commandes = $dao->findAllAssocFromIdWithAnnonceInfo($_SESSION['idCompte']);
+            $template = $this->getTwig()->load('commandes.html.twig');
+            echo $template->render([
+                'commandes' => $commandes,
+            ]);
         }
-
-        $template = $this->getTwig()->load('livraisons.html.twig');
-        echo $template->render([
-            'commandes' => $commandes,
-        ]);
     }
 }
